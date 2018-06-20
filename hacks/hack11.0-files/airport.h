ac2a2f48b7d23c07db45aa36e31677589658f95e
Chris Bourke <cbourke@cse.unl.edu>
2018-06-20 10:50:04 -0500

    added hack 11.0 and header file

---
+++ hacks/hack11.0-files/airport.h
@@ -0,0 +1,55 @@
+
+typedef struct {
+  // TODO: design your structure
+} Airport;
+
+/**
+ * A factory function to create a new Airport with the given
+ * attributes.  This function should make *deep* copies of each
+ * of the relevant fields and return a pointer to the newly
+ * created Airport structure.
+ */
+Airport* createAirport(const char* gpsId,
+                       const char* type,
+                       const char* name,
+                       double latitude,
+                       double longitude,
+                       int elevationFeet,
+                       const char* city,
+                       const char* countryAbbrv);
+
+/**
+ * This function initializes an existing allocated
+ * Airport structure with the given attributes.
+ */
+void initAirport(Airport* airport,
+                 const char* gpsId,
+                 const char* type,
+                 const char* name,
+                 double latitude,
+                 double longitude,
+                 int elevationFeet,
+                 const char* city,
+                 const char* countryAbbrv);
+
+/**
+ * Constructs a new string representation of the given
+ * Airport structure.
+ */
+char* airportToString(const Airport* a);
+
+/**
+ * Computes the air distance, in kilometers, between
+ * the two Airports using their latitude/longitude
+ */
+double getAirDistance(const Airport* origin, const Airport* destination);
+
+/**
+ * Computes the estimated travel time (in hours) for a flight
+ * that involves the given stops using the average flight speed
+ * (kilometers per hour) and average layover time (in hours)
+ */
+double getEstimatedTravelTime(const Airport* stops,
+                              int size,
+                              double aveKmsPerHour,
+                              double aveLayoverTimeHrs);
