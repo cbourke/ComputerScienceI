f0868234cfded3dd49078d0fade4b3c5fa2b7f48
Chris Bourke <cbourke@cse.unl.edu>
2020-10-09 12:39:06 -0500

    ported validation script

---
+++ scripts/codepost/codepostValidateCourse.py
@@ -0,0 +1,62 @@
+# This script simply lists some relevant information
+# associated with your codepost.io course
+import codepost
+from course import course
+from config import config
+
+codepost.configure_api_key(config.codePostApiKey)
+
+codePostCourse = codepost.course.retrieve(id=config.codePostCourseId)
+
+print("Retrieved course: ")
+print("Id     = " + str(codePostCourse.id))
+print("Name   = " + codePostCourse.name)
+print("Period = " + codePostCourse.period)
+
+print("Assignments (" + str(len(codePostCourse.assignments)) + "): ")
+for a in codePostCourse.assignments:
+  print("%s (id=%d)"%(a.name,a.id))
+
+roster = codepost.roster.retrieve(id=config.codePostCourseId)
+graders = list()
+students = list()
+
+print("Admins")
+for a in roster.courseAdmins:
+  print("  %s"%a)
+  graders.append(a);
+print("Graders")
+for g in roster.graders:
+  print("  %s"%g)
+  graders.append(g);
+graders = list(set(graders))
+print("Students")
+for s in roster.students:
+  print("  %s"%s)
+  students.append(s);
+
+# check that codepost graders are in canvas class
+print("Checking Instructors...")
+for nuid,instructor in course.instructors.items():
+  if instructor.canvasEmail not in graders:
+    print("Instructor %s not in codepost"%instructor)  
+  else:
+    graders.remove(instructor.canvasEmail)    
+if graders:
+  print("Instructors in codepost missing in canvas:")
+  for g in graders:
+    print("  %s"%g)
+    
+# check that codepost students are in canvas class
+print("Checking Students...")
+for nuid,student in course.students.items():
+  if student.canvasEmail not in students:
+    print("Student %s not in codepost"%student)  
+  else:
+    students.remove(student.canvasEmail)    
+if student:
+  print("Students in codepost missing in canvas:")
+  for s in students:
+    print("  %s"%s)    
+    
+    
