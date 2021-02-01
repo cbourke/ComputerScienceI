b9037ad4d4801d6a36e5179925ee626c156fd29b
Chris Bourke <>
2021-02-01 11:36:13 -0600

    doc format cleanup

--- scripts/codepost/group.py
+++ scripts/codepost/group.py
@@ -1,18 +1,18 @@
 from operator import attrgetter
 
-# A group may consist of one or more students
-# and has a direct connection to a group on Canvas 
-# in which students are either assigned or self
-# select into groups.  In the case that students
-# are not assigned to a group, a python Group
-# object is still created for them as a "group 
-# of one".
-#
-# Codepost does not represent groups directly, however
-# each submission may have more than one student 
-# assicated with it.
-#
 class Group:
+    """
+    A group may consist of one or more students and has a 
+    direct connection to a group on Canvas in which students 
+    are either assigned or self select into groups through
+    the "people" page.  In the case that students are not 
+    assigned to a group, a Group object is still created for 
+    them as a "group of one".
+    
+    Codepost does not represent groups directly, however each 
+    submission may have more than one student assicated with it.
+    """
+
     canvasGroupId   = None
     canvasGroupName = None
     # This is an array of Persons
@@ -22,6 +22,7 @@ class Group:
                        canvasGroupName = None):
         self.canvasGroupId   = canvasGroupId
         self.canvasGroupName = canvasGroupName
+        
     def __str__(self):
         s = ""
         if len(self.members) > 1:
@@ -32,8 +33,10 @@ class Group:
           for member in self.members:
             s += "%s\n"%(self.formatMember(member,"  "))
         return s
+        
     def __eq__(self,other):
         return self.members == other.members        
+
     def __lt__(self,other):
         return (self.members[0].name < other.members[0].name)
 
