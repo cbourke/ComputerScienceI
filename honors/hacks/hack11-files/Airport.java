f5295c1d722d7d29a76f4a2bf435724f08ee571b
Chris Bourke <>
2021-04-09 21:02:09 -0500

    added hack 11

---
+++ honors/hacks/hack11-files/Airport.java
@@ -0,0 +1,35 @@
+package unl.cse;
+
+import java.util.List;
+
+/**
+ * TODO: documentation
+ *
+ */
+public class Airport {
+
+	//TODO: define your member variables and methods
+
+	/**
+	 * Computes the air distance, in kilometers, from <code>this<code>
+	 * airport to the given <code>destination</code> using their latitude/longitude
+	 * and the
+	 * <a href="https://en.wikipedia.org/wiki/Spherical_law_of_cosines">Spherical
+	 * Law of Cosines</a>.
+	 */
+	public double getAirDistance(Airport destination) {
+		//TODO: implement
+		return -1.0;
+	}
+
+	/**
+	 * Computes the estimated travel time (in hours) for a flight that involves the
+	 * given stops using the average flight speed (kilometers per hour) and average
+	 * layover time (in hours)
+	 */
+	public static double getEstimatedTravelTime(List<Airport> stops, double aveKmsPerHour, double aveLayoverTimeHrs) {
+		//TODO: implement
+		return -1.0;
+	}
+
+}
