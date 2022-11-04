4d9109a833046d943aa9f1de8b69e9177416c564
Chris Bourke <cbourke@cse.unl.edu>
2022-11-03 19:53:08 -0500

    added example DNA/protein files

--- hacks/hack10.0/proteinUtilDemo.c
+++ hacks/hack10.0/proteinUtilDemo.c
@@ -12,13 +12,17 @@
 int main(int argc, char **argv) {
 
   if(argc != 2) {
-    fprintf(stderr, "Usage: %s RNA\n", argv[0]);
+    fprintf(stderr, "Usage: %s RNATrigram\n", argv[0]);
     exit(1);
   }
 
   char prot = rnaToProtein(argv[1]);
 
-  printf("%s -> %c\n", argv[1], prot);
+  if(prot == '\0') {
+    printf(" \"%s\" is an invalid RNA trigram\n", argv[1]);
+  } else {
+    printf("%s -> %c\n", argv[1], prot);
+  }
 
   return 0;
 }
