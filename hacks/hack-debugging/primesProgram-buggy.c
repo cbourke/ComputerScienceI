ee12336fd2d0008ca1467be3a909cb7aa4b7ed70
taqqisarra <147220821+taqqisarra@users.noreply.github.com>
2023-10-22 04:42:43 -0700

    Update primesProgram-buggy.c

--- hacks/hack-debugging/primesProgram-buggy.c
+++ hacks/hack-debugging/primesProgram-buggy.c
@@ -1,16 +1,10 @@
-/**
- * This program computes the sum of the first n
- * prime numbers.  Optionally, it allows the user
- * to provide n as a command line argument, but
- * defaults to the first n = 10 primes
- */
 #include <stdlib.h>
 #include <stdio.h>
 #include <math.h>
 
 /**
  * This function takes an integer array (of size n) and
- * returns the sum of its elements.  It returns 0 if the
+ * returns the sum of its elements. It returns 0 if the
  * array is NULL or if its size is <= 0
  */
 int sum(int *arr, int n);
@@ -22,56 +16,71 @@ int sum(int *arr, int n);
 int* getPrimes(int n);
 
 /**
- * This function determines returns true if the give
+ * This function determines and returns true if the given
  * integer is prime and false otherwise.
  */
 int isPrime(int x);
 
 int main(int argc, char **argv) {
+    int n = 10; // Default to the first 10 primes
 
-  int n = 10; //default to the first 10 primes
-  if(argc = 2) {
-    atoi(argv[2]);
-  }
-  int *primes = getPrimes(n);
+    if (argc == 2) {
+        n = atoi(argv[1]); // Parse n from the command line argument
+    }
 
-  int s = sum(primes, n);
-  printf("The sum of the first %d primes is %d\n", n, s);
+    int *primes = getPrimes(n);
+    int s = sum(primes, n);
 
-  return 0;
+    printf("The sum of the first %d primes is %d\n", n, s);
+
+    free(primes); // Free the dynamically allocated array
+
+    return 0;
 }
 
 int sum(int *arr, int n) {
-  int i;
-  int total;
-  for(i=0; i<n; i++) {
-    total =+ arr[i];
-  }
-  return total;
+    int i;
+    int total = 0; // Initialize total to 0
+    for (i = 0; i < n; i++) {
+        total += arr[i]; // Fix the += operator
+    }
+    return total;
 }
 
 int* getPrimes(int n) {
-  int result[n];
-  int i = 0;
-  int x = 2;
-  while(i < n) {
-    if(isPrime(x)) {
-      result[i] = x;
-      i++;
-      x += 2;
+    int *result = (int *)malloc(n * sizeof(int));
+    if (result == NULL) {
+        fprintf(stderr, "Memory allocation failed\n");
+        exit(1);
     }
-  }
-  return result;
+
+    int i = 0;
+    int x = 2;
+    while (i < n) {
+        if (isPrime(x)) {
+            result[i] = x;
+            i++;
+        }
+        x++;
+    }
+    return result;
 }
 
 int isPrime(int x) {
-  if(x % 2 == 0) {
-    return 0;
-  }
-  for(int i=3; i<=sqrt(x); i+=2) {
-    if(x % i == 0) {
-      return 0;
+    if (x < 2) {
+        return 0; // Numbers less than 2 are not prime
     }
-  }
-  return 1;
+    if (x == 2) {
+        return 1; // 2 is prime
+    }
+    if (x % 2 == 0) {
+        return 0; // Even numbers greater than 2 are not prime
+    }
+    for (int i = 3; i <= sqrt(x); i += 2) {
+        if (x % i == 0) {
+            return 0; // x is divisible by i, so it's not prime
+        }
+    }
+    return 1; // x is prime
 }
+
