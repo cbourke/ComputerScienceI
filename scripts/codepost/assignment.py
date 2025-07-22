c7e5e0ddde1930e96d667f7afea230237795ae97
Chris Bourke <cbourke@cse.unl.edu>
2025-07-21 19:14:30 -0500

    updated assignment class

--- scripts/codepost/assignment.py
+++ scripts/codepost/assignment.py
@@ -1,3 +1,6 @@
+"""
+A single class to model an assignment in Canvas
+"""
 
 class Assignment:
     """
@@ -5,34 +8,34 @@ class Assignment:
     minimum of data
     """
 
-    id = None
-    name = None
-    points = None
+    assignment_id = None
+    name          = None
+    points        = None
 
-    def __init__(self, id,
+    def __init__(self, assignment_id,
                        name = None,
                        points = None):
-        self.id     = id
+        self.assignment_id     = assignment_id
         self.name   = name
         self.points = points
 
     def __str__(self):
-        return f"{self.name} ({self.id}) {self.points:.1f} pts"
+        return f"{self.name} ({self.assignment_id}) {self.points:.1f} pts"
 
     def __repr__(self):
-        return f"{self.name} ({self.id}) {self.points:.1f} pts"
+        return f"{self.name} ({self.assignment_id}) {self.points:.1f} pts"
 
     def __hash__(self):
-        return hash(self.id)
+        return hash(self.assignment_id)
 
     def __eq__(self,other):
         """
         Equality and ordering is determined based only on (canvas) id
         """
-        return self.id == other.id
+        return self.assignment_id == other.assignment_id
 
     def __lt__(self,other):
         """
         Equality and ordering is determined based only on (canvas) id
         """
-        return (self.id < other.id)
+        return self.assignment_id < other.assignment_id
